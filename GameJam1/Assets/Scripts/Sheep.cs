421cde1db4f7dc9b0c75c4ca8f92c2a57985c571
ChrisHorrigan <cdhorrigan@gmail.com>
2015-09-25 21:48:33 -0700

    Sheep Movement, created GameControl script

---
+++ GameJam1/Assets/Scripts/Sheep.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sheep : MonoBehaviour {
+    NavMeshAgent agent;
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        StartCoroutine(RandomMovement());
+	}
+    IEnumerator RandomMovement()
+    {
+        Vector3 toMove;
+        yield return new WaitForSeconds(Random.Range(1f, 3f));
+        toMove.y = 0;
+        toMove.x = Random.Range(-1f, 1f);
+        toMove.z = Random.Range(-1f, 1f);
+        agent.SetDestination(transform.position + toMove);
+       
+        StartCoroutine(RandomMovement());
+
+    }
+    public void GetWhistled()
+    {
+        StopCoroutine(RandomMovement());
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
